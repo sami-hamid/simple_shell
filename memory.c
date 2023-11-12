df5102e321eced8e4f804091aa20a8f4501a150e
sami-hamid <samifadlallah21@gmail.com>
2023-11-12 09:12:33 -0800

    simple_shell

---
+++ memory.c
@@ -0,0 +1,19 @@
+memory.c
+#include "shell.h"
+
+/**
+ * bfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
